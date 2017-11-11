f50ca29f0a01fd2e827e9e9e80276b49b9257a88
Oscar Antonio Duran Grillo <hi@oduran.me>
2017-11-11 21:31:07 +0100

    Add mappable protocol definition

---
+++ Sources/CoreService/Core/Infrastructure/Protocol/Mapper/Mappable.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+public protocol Mappable {
+  associatedtype From
+  associatedtype To
+  
+  func map(_ from: From) throws -> To
+}
+
+// MARK: - Mappable + Optionals
+
+public extension Mappable {
+  public func map(_ from: From?) throws -> To? {
+    guard let from = from else { return nil }
+    return try map(from)
+  }
+}
+
+// MARK: - Mappable + Array
+
+public extension Mappable {
+  public func map(elements: [From]) throws -> [To] {
+    return try elements.map { element -> To in
+      return try map(element)
+    }
+  }
+}
