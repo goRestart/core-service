c87756027f27a814812ce8f5ac69a89f995ba6f1
Oscar Antonio Duran Grillo <hi@oduran.me>
2017-11-12 13:55:54 +0100

    Extends filter so we get the like function

--- Sources/CoreService/Core/Infrastructure/Extension/QueryRepresentable/QueryRepresentable+Filter.swift
+++ Sources/CoreService/Core/Infrastructure/Extension/QueryRepresentable/QueryRepresentable+Filter.swift
@@ -5,4 +5,9 @@ extension QueryRepresentable where Self: ExecutorRepresentable {
     let query = "LOWER(\(field)) = LOWER('\(to)')"
     return try makeQuery().filter(raw: query)
   }
+  
+  public func filter(_ field: String, like: String) throws -> Query<Self.E> {
+    let query = "LOWER(\(field)) ILIKE LOWER('%\(like)%')"
+    return try makeQuery().filter(raw: query)
+  }
 }
