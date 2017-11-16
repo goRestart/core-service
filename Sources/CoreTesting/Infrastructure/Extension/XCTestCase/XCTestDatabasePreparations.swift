9554e0e30e73f58951708884c0d67c63dd392edf
Oscar Antonio Duran Grillo <hi@oduran.me>
2017-11-16 20:28:58 +0100

    Simplify the database preparation class

--- Sources/CoreTesting/Infrastructure/Extension/XCTestCase/XCTestDatabasePreparations.swift
+++ Sources/CoreTesting/Infrastructure/Extension/XCTestCase/XCTestDatabasePreparations.swift
@@ -2,33 +2,16 @@ import XCTest
 import Vapor
 import FluentProvider
 import Testing
+import Fluent
 
 open class XCTestDatabasePreparations: XCTestCase {
   
-  public var database: Database!
-  public var droplet: Droplet?
-
-  override open func setUp() {
-    super.setUp()
+  public var droplet: Droplet!
+  
+  public func prepare(_ droplet: Droplet) {
+    self.droplet = droplet
+    
     Fluent.autoForeignKeys = false
     Testing.onFail = XCTFail
-    
-    let driver = try! MemoryDriver()
-    database = Database(driver)
-    
-    droplet?.config.arguments = ["vapor", "--env=test"]
-    
-    initializeDatabase()
-  }
-  
-  override open func tearDown() {
-    super.tearDown()
-    guard let droplet = droplet else { return }
-    try! database.revertAll(droplet.config.preparations)
-  }
-  
-  private func initializeDatabase() {
-    guard let droplet = droplet else { return }
-    try! database.prepare(droplet.config.preparations)
   }
 }
